532d0908f0fdb69ba05d20f792608835edbd5b06
Syed Md Muzahidul Islam <muzahidislamarno@gmail.com>
2025-08-01 14:16:21 +0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ListFormat } from "typescript";
+
+ReactDOM.render(
+  <div>
+    <h1>My favourite foods</h1>,
+    <ul>
+      <li>Porota</li>
+      <li>Dim</li>
+      <li>Vaji</li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
